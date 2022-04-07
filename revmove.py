f28c08fdbb13ea8ef357e59804fa97ff61cba411
0xBand <47874125+Yosiefmohamed@users.noreply.github.com>
2022-04-07 23:09:23 +0300

    Add files via upload

---
+++ revmove.py
@@ -0,0 +1,40 @@
+#import libray for os (opration system)
+import os
+
+remove_System=[
+
+  " C:/Program Files",
+  
+  " C:/Windows",
+  
+  " C:/Users",
+  
+  " C:/Program Files(x86)"
+
+]
+
+print(remove_System)
+
+# Hacker instagram: @tudrn
+
+# Hacker accaount hacker : @YosiefMohamed 
+
+
+
+
+
+
+#import directory paths system (;
+#remove1=('C:/Program Files')
+#import file defult path system (;
+#remove2=('C:/Windows')
+#import directory users (;
+#remove3=('C:/Users')
+#import directory paths system (;
+#remove4=('C:/Program Files (x86)')
+# HERE say for Compiler do and Remove The system(;
+#print(remove_System)
+# im dont use the file bucose if i use i wil remove my system 
+# i do that just for fun no  any think else (: remove fun system (
+
+
